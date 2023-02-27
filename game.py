39e43b448ec3b098fcd1ac7417f1743d07e97365
Maxim <info.max.lapin@gmail.com>
2023-02-27 10:33:19 +0100

    Initial commit

---
+++ game.py
@@ -0,0 +1,23 @@
+"""Guess a number"""
+
+import numpy as np
+
+number = np.random.randint(1, 101) # random number is chosen
+
+# Number of tries
+
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Guess a number from 1 to 100!\n"))
+    
+    if predict_number > number:
+        print("Lower!")
+        
+    elif predict_number < number:
+        print("Higher!")
+    
+    else:
+        print(f"Congratulation! It was number {number}. You got it in {count} tries!")
+        break # End of the game
